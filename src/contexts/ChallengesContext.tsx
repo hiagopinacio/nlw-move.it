96479ed517a168052bac12f30a0acdc9a35fd509
Hiago Paiva Inacio <hiagopinacio@gmail.com>
2021-02-27 12:34:54 -0300

    Aula 3: Definindo desafio ao finalizar ciclo.

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,13 +1,19 @@
 import { createContext, useState, ReactNode } from "react";
 
+import challenges from "../../challenges.json"
 
-
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
 interface ChallengesContextData {
     level: number,
     currentExperience: number,
     challengesCompleted: number,
     levelUp: () => void,
     startNewChallenge: () => void,
+    activeChallenge: Challenge,
 }
 
 interface ChallengesProviderProps {
@@ -21,17 +27,21 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [currentExperience, setCurrentExperience] = useState(0)
     const [challengesCompleted, setChallengesCompleted] = useState(0)
 
+    const [activeChallenge, setActiveChallenge] = useState(null)
+
     function levelUp() {
         setLevel(level + 1)
     }
 
     function startNewChallenge() {
-        console.log('new challenge');
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+        const challenge = challenges[randomChallengeIndex]
 
+        setActiveChallenge(challenge)
     }
 
     return (
-        <ChallengesContext.Provider value={{ level, currentExperience, challengesCompleted, levelUp, startNewChallenge }}>
+        <ChallengesContext.Provider value={{ level, currentExperience, challengesCompleted, levelUp, startNewChallenge, activeChallenge }}>
             {children}
         </ChallengesContext.Provider>
     )
