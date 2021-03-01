3e159819b2fc20b6427963d81a6c468140cbc77e
Hiago Paiva Inacio <hiagopinacio@gmail.com>
2021-02-28 22:17:41 -0300

    Aula 4: Requisitando Permissão e criando notificação.

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from "react";
+import { createContext, useState, ReactNode, useEffect } from "react";
 
 import challenges from "../../challenges.json"
 
@@ -34,6 +34,11 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
     const experieceToNextLevel = Math.pow((level + 1) * 4, 2)
 
+    useEffect(() => {
+        Notification.requestPermission()
+    }, [])
+
+
     function levelUp() {
         setLevel(level + 1)
     }
@@ -43,6 +48,11 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         const challenge = challenges[randomChallengeIndex]
 
         setActiveChallenge(challenge)
+        if (Notification.permission === 'granted') {
+            new Notification('Novo desafio 🎉🎉', {
+                body: `Valendo ${challenge.amount}xp!`
+            })
+        }
     }
 
     function resetChallenge() {
