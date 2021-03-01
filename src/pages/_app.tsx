361bf1415f89e4e477466b8f41d09e9879e1c90b
Hiago Paiva Inacio <hiagopinacio@gmail.com>
2021-02-28 23:00:39 -0300

    Aula 5: Armazenando dados em cookies.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,12 +10,11 @@ function MyApp({ Component, pageProps }) {
 
 
   return (
-    <ChallengesProvider>
 
 
-      <Component {...pageProps} />
 
-    </ChallengesProvider>
+    <Component {...pageProps} />
+
   )
 }
 
